01f37a08ea2224a2aba4980e836f255ee6f83c1a
Alvvvvays <56679385+Alvvvvays@users.noreply.github.com>
2022-11-04 12:59:15 +0000

    Fix memory usage in large datasets

--- eda/01_data_exploration.py
+++ eda/01_data_exploration.py
@@ -61,3 +61,4 @@ plt.savefig('../output/distributions.png')
 # Updated logic for better readability
 // Added: new utility function
 // Added: new utility function
+# Fix: handle None values properly
